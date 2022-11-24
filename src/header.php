3bf87fc0b6d67ca6c19a4f217438ac2519317d94
Patryk Jankowski <patryk.jankowski@protonmail.com>
2022-11-24 21:21:30 +0100

    pages updated, callpage added

--- src/header.php
+++ src/header.php
@@ -18,15 +18,15 @@
     <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap">
     <link rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');" href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap">
 
-    <link href="https://www.googletagmanager.com/gtm.js?id=GTM-WSB34T7" rel="preload" as="script">
-    <link rel="dns-prefetch" href="https://www.googletagmanager.com/" >
+    <!--<link href="https://www.googletagmanager.com/gtm.js?id=GTM-KB22WNS" rel="preload" as="script">
+    <link rel="dns-prefetch" href="https://www.googletagmanager.com/" >-->
 
     <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <!--<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WSB34T7');</script>
+        })(window,document,'script','dataLayer','GTM-KB22WNS');</script>-->
     <!-- End Google Tag Manager -->
 
     <?php wp_head(); ?>
--- src/page-sygnalisci2.php
+++ src/page-sygnalisci2.php
@@ -13,7 +13,7 @@
                 </p>
                 <p>Kompleksowo zajmujemy się wdrożeniem wymagań nowych przepisów prawa w Twojej firmie!</p>
                 <p>Do Twojej dyspozycji udostępniamy przejrzysty i intuicyjny system zgłoszeń. Nasi specjaliści zajmą się obsłużeniem i rozwiązaniem sprawy. Przeprowadzimy Cię także przez wszystkie formalności.</p>
-                <a class="button button--300 font-size-20 mt-20" href="/kontakt">Dowiedz się więcej</a>
+                <a class="button button--300 font-size-20 mt-20" href="#cp-widget">Zapytaj o ofertę</a>
             </div>
             <div class="col-xl-6 mt-0-xl">
                 <div class="row">
@@ -25,9 +25,18 @@
 </section>
 
 <section class="section hide-max-lg">
-    <a href="/kontakt">
+    <a href="#cp-widget">
         <img src="/wp-content/themes/perfectinfo/img/sygnalisci.webp" style="height: 100%; margin: 0 auto;display: block;">
     </a>
+</section>
+
+<section class="section pt-40 pb-40">
+    <div class="container">
+        <div class="row">
+            <p class="font-size-18 font-size-24-lg text-align-center">"PROMOCJA! Abonament na Platformę Perfecti Whistle (Szkolenia, Procedury, System zgłoszeń) za 1zł/msc do momentu opublikowania finalnej ustawy o ochronie osób zgłaszających naruszenia"</p>
+            <a class="button button--priamry button--300 font-size-20 mt-20 margin-auto" href="#cp-widget">Zapytaj o ofertę</a>
+        </div>
+    </div>
 </section>
 
 <section class="section" id="dyrektywa">
@@ -36,33 +45,22 @@
             <div class="col-lg-4 d-flex flex-column">
                 <div class="col-border">
                     <svg xmlns="http://www.w3.org/2000/svg" class="mb-20 margin-auto" viewBox="0 0 35.357 35.357" width="70" height="70" fill="#029ddd"><path d="M30.664 14.623h-5.613V8.894a6.224 6.224 0 0 1 5.613 5.729zm-6.673-5.515a6.224 6.224 0 0 0-6.109 6.221 6.23 6.23 0 0 0 6.227 6.229 6.22 6.22 0 0 0 6.214-5.992h-6.332V9.108zm-.798 17.204v1.834h1.152v1.688H11.013v-1.688h1.152v-1.834H0V5.524h35.357v20.787H23.193zm-19.305-1.5h2.793v-.952a.246.246 0 1 1 .493 0v.951h6.166v-.951a.246.246 0 1 1 .492 0v.951h2.792a.6.6 0 0 0 .071-.404l-.432-2.428c-.08-.445-.521-.9-.965-.996l-.304-.062-1.32-.277-.912 1.062-2.278 2.662a.236.236 0 0 1-.184.084h-.001a.24.24 0 0 1-.185-.082l-2.347-2.664-.935-1.062-1.314.277-.305.062c-.444.095-.885.55-.964.994l-.433 2.43a.62.62 0 0 0 .072.405zm7.272-8.893v-1.918l-.308 1.845H7.179v.545c-.062-.033-.124-.049-.184-.029-.203.06-.29.428-.197.822.09.387.55.654.748.608.209.688.68 1.411 1.229 1.937.077.072.152.139.229.203.087.07.173.139.263.197l-1.882.396 1.041 1.18 1.237 1.404.306-1.404.045-.198-.458-.455.572-.57.128-.129.127.125.574.574-.457.455.044.198.319 1.462 1.25-1.462 1.011-1.18-1.893-.397c.086-.062.174-.127.261-.197.076-.064.153-.133.229-.205.557-.529 1.042-1.258 1.25-1.932.197.042.653-.225.744-.608.092-.395.005-.764-.198-.823-.06-.019-.122-.002-.184.03v-.473l-2.173-.001zm22.698 8.893V7.024H1.5v17.787h1.849a1.157 1.157 0 0 1-.017-.492l.433-2.426c.114-.646.707-1.258 1.348-1.393l.406-.089v-4.319c0-4.013 2.122-5.535 4.738-5.535 2.617 0 4.739 1.522 4.739 5.535v4.319l.406.089c.641.135 1.234.747 1.347 1.393l.434 2.426c.03.172.022.338-.018.492h16.693z"/></svg>
-                    <h3 class="font-size-24 font-weight-400 text-align-center color-primary mb-20 mt-20" style="min-height: 58px">Sygnaliści – dlaczego trzeba wdrożyć?</h3>
-                    <p class="text-align-center">Unia Europejska w 2019 ustanowiła Dyrektywę o ochronie sygnalistów, która nakazuje organizacjom z sektora publicznego oraz prywatnego, wdrożenie systemu, w którym informator, (w tym wypadku tzw. „Sygnalista”) będzie miał możliwość zgłoszenia nieprawidłowości w danej firmie. Sygnalistą może być pracownik, partner biznesowy lub kandydat do pracy. Każdy z nich miał możliwość złożyć skargę podając swoje dane lub pozostając anonimowy. Celem ustawy jest ochrona osób zgłaszających naruszenia i poprawa w zakresie przestrzegania prawa i wartości etycznych w organizacjach.</p>
+                    <h3 class="font-size-24 font-weight-400 text-align-center color-primary mb-20 mt-20" style="min-height: 58px"><?php echo get_field('sygnalisci_s1_c1_header'); ?></h3>
+                    <div class="text-align-center"><?php echo get_field('sygnalisci_s1_c1_text'); ?></div>
                 </div>
             </div>
             <div class="col-lg-4 d-flex flex-column">
                 <div class="col-border">
                     <svg xmlns="http://www.w3.org/2000/svg" class="mb-20 margin-auto" viewBox="0 0 508.05 508.05" width="70" height="70" fill="#029ddd"><path d="M493.925 177.95h-105.5v-58.4h20.9c6.4 0 11.9-4.3 13.6-10.4 1.7-6.1-1-12.6-6.5-15.9l-155.3-91.3c-4.4-2.6-9.9-2.6-14.3 0l-155.4 91.2c-5.5 3.2-8.1 9.7-6.5 15.9 1.7 6.1 7.3 10.4 13.6 10.4h20.9v58.4h-105.3c-7.8.1-14.1 6.4-14.1 14.2v301.9c0 7.8 6.3 14.1 14.1 14.1h479.8c7.8 0 14.1-6.3 14.1-14.1v-301.9c0-7.8-6.3-14.1-14.1-14.1zm-239.9-147.4 103.5 60.7h-207l103.5-60.7zm-134.5 449.3h-91.3v-273.7h91.3v273.7zm166.4 0h-63.9v-93.3h63.9v93.3zm28.2 0v-107.5c0-7.8-6.3-14.1-14.1-14.1h-92.1c-7.8 0-14.1 6.3-14.1 14.1v107.4h-46.1V119.55h212.4v360.3h-46zm165.7 0h-91.3v-273.7h91.3v273.7z"/><path d="M78.825 240.75h-9.8c-7.8 0-14.1 6.3-14.1 14.1 0 7.7 6.3 14.1 14.1 14.1h9.8c7.8 0 14.1-6.3 14.1-14.1s-6.3-14.1-14.1-14.1zM78.825 299.45h-9.8c-7.8 0-14.1 6.3-14.1 14.1s6.3 14.1 14.1 14.1h9.8c7.8 0 14.1-6.3 14.1-14.1s-6.3-14.1-14.1-14.1zM78.825 358.25h-9.8c-7.8 0-14.1 6.3-14.1 14.1s6.3 14.1 14.1 14.1h9.8c7.8 0 14.1-6.3 14.1-14.1s-6.3-14.1-14.1-14.1zM78.825 417.05h-9.8c-7.8 0-14.1 6.3-14.1 14.1s6.3 14.1 14.1 14.1h9.8c7.8 0 14.1-6.3 14.1-14.1s-6.3-14.1-14.1-14.1zM439.025 240.75h-9.8c-7.8 0-14.1 6.3-14.1 14.1 0 7.7 6.3 14.1 14.1 14.1h9.8c7.8 0 14.1-6.3 14.1-14.1s-6.3-14.1-14.1-14.1zM439.025 299.45h-9.8c-7.8 0-14.1 6.3-14.1 14.1s6.3 14.1 14.1 14.1h9.8c7.8 0 14.1-6.3 14.1-14.1s-6.3-14.1-14.1-14.1zM439.025 358.25h-9.8c-7.8 0-14.1 6.3-14.1 14.1s6.3 14.1 14.1 14.1h9.8c7.8 0 14.1-6.3 14.1-14.1s-6.3-14.1-14.1-14.1zM439.025 417.05h-9.8c-7.8 0-14.1 6.3-14.1 14.1s6.3 14.1 14.1 14.1h9.8c7.8 0 14.1-6.3 14.1-14.1s-6.3-14.1-14.1-14.1zM254.025 147.15c-44.8 0-81.2 36.4-81.2 81.2s36.4 81.2 81.2 81.2 81.2-36.4 81.2-81.2-36.4-81.2-81.2-81.2zm0 134.1c-29.1 0-53-23.6-53-52.8s23.8-53 53-53 52.9 23.8 52.9 52.9-23.8 52.9-52.9 52.9z"/><path d="M271.725 217.45h-3.5v-15c0-7.8-6.3-14.1-14.1-14.1s-14.1 6.3-14.1 14.1v29.1c-.1 7.7 6.2 14.1 14 14.1h17.7c7.8 0 14.1-6.3 14.1-14.1s-6.3-14.1-14.1-14.1z"/></svg>
-                    <h3 class="font-size-24 font-weight-400 text-align-center color-primary mb-20 mt-20" style="min-height: 58px">Kogo dotyczy Dyrektywa?</h3>
-                    <p class="text-align-center"><strong>Od 17.12.2021r.</strong>: organizacji z sektora prywatnego zatrudniających powyżej 249 pracowników, organizacji z sektora publicznego zatrudniających powyżej 50 pracowników</p>
-
-                    <p class="text-align-center"><strong>od 17.12.2023r.</strong>: organizacji z sektora prywatnego zatrudniających powyżej 50 pracowników</p>
-
-                    <p class="text-align-center">Podane terminy to <b>daty, w których system musi być już wdrożony w organizacji.</b></p>
+                    <h3 class="font-size-24 font-weight-400 text-align-center color-primary mb-20 mt-20" style="min-height: 58px"><?php echo get_field('sygnalisci_s1_c2_header'); ?></h3>
+                    <div class="text-align-center"><?php echo get_field('sygnalisci_s1_c2_text'); ?></div>
                 </div>
             </div>
             <div class="col-lg-4 d-flex flex-column">
                 <div class="col-border">
-                <svg xmlns="http://www.w3.org/2000/svg" class="mb-20 margin-auto" viewBox="0 0 206.003 206.003" width="70" height="70" fill="#029ddd"><path d="M103.002 206.003a5.995 5.995 0 0 1-3.175-.909C43.867 170.2 22.4 126.118 14.246 95.282 5.527 62.313 9.67 37.499 10.002 35.636l.096-.596a5.999 5.999 0 0 1 5.829-5.05c.301-.007 32.023-.993 58.541-28.18a6.001 6.001 0 0 1 6.91-1.211c6.77 3.277 14.045 4.938 21.624 4.938s14.854-1.662 21.624-4.938a6 6 0 0 1 6.91 1.211c26.688 27.362 58.226 28.175 58.541 28.18a5.999 5.999 0 0 1 5.829 5.05l.096.596c.333 1.863 4.475 26.677-4.244 59.646-8.154 30.836-29.621 74.918-85.581 109.813a6.005 6.005 0 0 1-3.175.908zM21.328 41.569c-.8 7.938-1.717 27.418 4.667 51.203 10.871 40.5 36.77 74.169 77.005 100.126 50.261-32.47 69.711-72.531 77.156-100.684 6.254-23.649 5.334-42.796 4.528-50.644-10.673-1.301-35.839-6.633-58.756-28.433-7.276 2.922-14.969 4.401-22.927 4.401S87.35 16.06 80.074 13.137C57.165 34.93 32.007 40.265 21.328 41.569z"/><path d="M103.002 168.796a5.995 5.995 0 0 1-3.175-.909c-35.771-22.305-49.492-51.723-54.705-72.474-5.662-22.54-2.809-39.755-2.685-40.477l.055-.343a5.999 5.999 0 0 1 5.829-5.05c.165-.004 19.228-.662 35.174-17.011a6.001 6.001 0 0 1 6.91-1.211c3.943 1.909 8.181 2.876 12.597 2.876s8.654-.968 12.597-2.876a6 6 0 0 1 6.91 1.211c15.946 16.349 35.01 17.006 35.201 17.011 2.891.071 5.346 2.195 5.803 5.05l.066.412c.113.653 2.965 17.868-2.696 40.408-5.212 20.751-18.935 50.169-54.705 72.474a6.003 6.003 0 0 1-3.176.909zM53.843 60.998c-.428 5.966-.575 17.884 3.046 31.998 6.598 25.72 22.105 46.789 46.113 62.67 24.007-15.881 39.515-36.95 46.113-62.67 3.625-14.129 3.474-26.057 3.047-31.998-7.563-1.195-21.931-5.014-35.276-17.213a40.626 40.626 0 0 1-13.884 2.413c-4.798 0-9.451-.81-13.884-2.413-13.345 12.198-27.712 16.017-35.275 17.213z"/><path d="m103.002 62.686 6.124 18.848h19.818l-16.033 11.649 6.124 18.848-16.033-11.649-16.033 11.649 6.124-18.848L77.06 81.534h19.818z"/></svg>
-                <h3 class="font-size-24 font-weight-400 text-align-center color-primary mb-20 mt-20" style="min-height: 58px">Co dalej? – obowiązki pracodawcy</h3>
-                <p class="text-align-center">Pracodawca zobowiązany jest do przygotowania:</p>
-                    <ul class="text-align-center">
-                        <li>Poufnego kanału zgłoszeń</li>
-                        <li>Odpowiednich procedur i regulaminów</li>
-                        <li>Przeszkolenia pracowników i współpracowników</li>
-                        <li>Wyznaczenia niezależnej osoby lub podmiotu obsługującego zgłoszenia</li>
-                        <li>Podejmowanie działań wyjaśniających</li>
-                    </ul>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mb-20 margin-auto" viewBox="0 0 206.003 206.003" width="70" height="70" fill="#029ddd"><path d="M103.002 206.003a5.995 5.995 0 0 1-3.175-.909C43.867 170.2 22.4 126.118 14.246 95.282 5.527 62.313 9.67 37.499 10.002 35.636l.096-.596a5.999 5.999 0 0 1 5.829-5.05c.301-.007 32.023-.993 58.541-28.18a6.001 6.001 0 0 1 6.91-1.211c6.77 3.277 14.045 4.938 21.624 4.938s14.854-1.662 21.624-4.938a6 6 0 0 1 6.91 1.211c26.688 27.362 58.226 28.175 58.541 28.18a5.999 5.999 0 0 1 5.829 5.05l.096.596c.333 1.863 4.475 26.677-4.244 59.646-8.154 30.836-29.621 74.918-85.581 109.813a6.005 6.005 0 0 1-3.175.908zM21.328 41.569c-.8 7.938-1.717 27.418 4.667 51.203 10.871 40.5 36.77 74.169 77.005 100.126 50.261-32.47 69.711-72.531 77.156-100.684 6.254-23.649 5.334-42.796 4.528-50.644-10.673-1.301-35.839-6.633-58.756-28.433-7.276 2.922-14.969 4.401-22.927 4.401S87.35 16.06 80.074 13.137C57.165 34.93 32.007 40.265 21.328 41.569z"/><path d="M103.002 168.796a5.995 5.995 0 0 1-3.175-.909c-35.771-22.305-49.492-51.723-54.705-72.474-5.662-22.54-2.809-39.755-2.685-40.477l.055-.343a5.999 5.999 0 0 1 5.829-5.05c.165-.004 19.228-.662 35.174-17.011a6.001 6.001 0 0 1 6.91-1.211c3.943 1.909 8.181 2.876 12.597 2.876s8.654-.968 12.597-2.876a6 6 0 0 1 6.91 1.211c15.946 16.349 35.01 17.006 35.201 17.011 2.891.071 5.346 2.195 5.803 5.05l.066.412c.113.653 2.965 17.868-2.696 40.408-5.212 20.751-18.935 50.169-54.705 72.474a6.003 6.003 0 0 1-3.176.909zM53.843 60.998c-.428 5.966-.575 17.884 3.046 31.998 6.598 25.72 22.105 46.789 46.113 62.67 24.007-15.881 39.515-36.95 46.113-62.67 3.625-14.129 3.474-26.057 3.047-31.998-7.563-1.195-21.931-5.014-35.276-17.213a40.626 40.626 0 0 1-13.884 2.413c-4.798 0-9.451-.81-13.884-2.413-13.345 12.198-27.712 16.017-35.275 17.213z"/><path d="m103.002 62.686 6.124 18.848h19.818l-16.033 11.649 6.124 18.848-16.033-11.649-16.033 11.649 6.124-18.848L77.06 81.534h19.818z"/></svg>
+                    <h3 class="font-size-24 font-weight-400 text-align-center color-primary mb-20 mt-20" style="min-height: 58px"><?php echo get_field('sygnalisci_s1_c3_header'); ?></h3>
+                    <div class="text-align-center"><?php echo get_field('sygnalisci_s1_c3_text'); ?></div>
                 </div>
             </div>
         </div>
@@ -73,7 +71,7 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h2 class="font-size-20 font-size-44-lg font-weight-300 color-white text-align-center mb-20 mt-20">Jako jedyni na rynku, dostarczamy prawdziwie kompleksowe rozwiązanie. Dzięki platformie Perfect Whistle otrzymasz dostęp do systemu zgłoszeń online, szkoleń oraz uszytej na Twoją miarę dokumentacji. Ty skupisz się na biznesie a my zajmiemy się wszystkim aspektami prawnymi, organizacyjnymi i technicznymi od A do Z.</h2>
+                <h2 class="font-size-20 font-size-44-lg font-weight-300 color-white text-align-center mb-20 mt-20"><?php echo get_field('sygnalisci_s2_text'); ?></h2>
             </div>
             <div class="row">
                 <div class="col">
@@ -88,8 +86,8 @@
 <section class="section">
     <div class="container">
         <div class="row">
-            <h2 class="font-size-36 font-size-44-lg font-weight-300 mb-70 color-primary">Jak to działa?</h2>
-            <img src="/wp-content/themes/perfectinfo/img/info.webp" class="mb-70 h-100 margin-auto" style="max-width: 960px">
+            <h2 class="font-size-36 font-size-44-lg font-weight-300 mb-70 color-primary"><?php echo get_field('sygnalisci_s3_header'); ?>Jak to działa?</h2>
+            <img src="/wp-content/themes/perfectinfo/img/info.webp" class="mb-70 h-100 margin-auto" style="max-width: 1060px">
             <!--<div class="col-xl-3 d-flex flex-column align-items-center text-align-center">
                 <div class="border-radius-50 p-10 bg-blue d-flex mb-20 mt-70 relative">
                     <div class="step-line hide-max-xl"><div class="step-arrow-right"></div></div>
@@ -331,7 +329,7 @@
             </div>
             <div class="col-lg-6">
                 <div class="row align-content-center justify-content-center h-100">
-                    <a class="button button--300 font-size-20 mt-20 color-black bg-white" href="/kontakt">
+                    <a class="button button--300 font-size-20 mt-20 color-black bg-white" href="#cp-widget">
                         <svg class="mr-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 59.997 59.997" height="40"><path fill="#3a3a3a" d="M44.071 38.385c-1.275-.728-2.597-.958-3.721-.646-.844.234-1.532.768-1.995 1.546-1.021 1.221-2.287 2.646-2.592 2.867-2.371 1.604-4.252 1.414-6.295-.629L17.982 30.037c-2.045-2.045-2.233-3.928-.631-6.292.224-.31 1.649-1.575 2.869-2.595.778-.464 1.312-1.151 1.546-1.996.311-1.123.081-2.444-.653-3.731-.173-.296-4.289-7.27-8.085-9.277a4.907 4.907 0 0 0-5.796.872L4.695 9.555c-7.401 7.402-6.031 16.596 3.962 26.588l14.705 14.706c4.539 4.54 8.937 7.326 13.068 8.281 1.096.253 2.17.38 3.224.38 3.717 0 7.169-1.573 10.296-4.7l2.537-2.537a4.907 4.907 0 0 0 .872-5.796c-2.008-3.795-8.981-7.913-9.288-8.092zm7.002 12.473-2.537 2.537c-6.568 6.568-14.563 5.237-23.76-3.961L10.071 34.728c-9.197-9.196-10.53-17.19-3.962-23.76l2.537-2.537a2.924 2.924 0 0 1 3.446-.518c3.294 1.743 7.256 8.454 7.289 8.511.449.787.62 1.608.457 2.196-.1.36-.323.634-.684.836l-.15.104c-.853.712-2.883 2.434-3.308 3.061-.612.904-1.018 1.792-1.231 2.665-.711-1.418-1.285-3.06-1.474-4.881a1.007 1.007 0 0 0-1.099-.892A1.003 1.003 0 0 0 11 20.612c.722 6.953 6.13 11.479 6.359 11.668.025.021.055.028.081.046l10.612 10.612c.045.045.091.084.136.127.035.052.058.109.104.155.189.187 4.704 4.567 11.599 5.283a1.001 1.001 0 0 0 .206-1.99c-2.115-.219-3.988-.839-5.549-1.558.766-.23 1.545-.612 2.334-1.147.627-.426 2.349-2.454 3.06-3.307l.104-.151c.202-.359.476-.583.836-.684.585-.159 1.408.007 2.185.45 1.879 1.102 7.073 4.56 8.521 7.296a2.913 2.913 0 0 1-.515 3.446zM27.362.487a1 1 0 1 0 0 2c16.892 0 30.635 13.743 30.635 30.635a1 1 0 1 0 2 0c0-17.995-14.64-32.635-32.635-32.635z"/><path fill="#3a3a3a" d="M27.362 9.517c13.016 0 23.604 10.589 23.604 23.604a1 1 0 1 0 2 0c0-14.118-11.486-25.604-25.604-25.604a1 1 0 1 0 0 2z"/><path fill="#3a3a3a" d="M27.362 16.547c9.14 0 16.575 7.436 16.575 16.575a1 1 0 1 0 2 0c0-10.242-8.333-18.575-18.575-18.575a1 1 0 1 0 0 2z"/></svg>
                         Chcę ofertę
                     </a>
